a389a5a9a425494d042a784740975b1a4ae53fbd
rummanis <rummanoffice12@gmail.com>
2021-10-26 18:25:51 +0600

    okdone

--- apis/admin.py
+++ apis/admin.py
@@ -6,4 +6,4 @@ from .models import GeeksModel
 
 @admin.register(GeeksModel)
 class GeeksModelAdmin(admin.ModelAdmin):
-    list_display=['id','title','description','price','image_main','image_1','image_2','image_link','image_link_1','image_link_2']
+    list_display=['id','title','description','price','image_main','image_1','image_2','image_link','image_link_1','image_link_2','category','countInStock']
